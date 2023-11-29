60255dbbaf16e4aa9ade651f06fc5dc0451355d0
hoangQuan25 <quan.nh215127@sis.hust.edu.vn>
2023-11-29 22:46:23 +0700

    first commit

---
+++ app/register/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <form className='text-center'>
+        <div className='flex flex-col justify-center h-[450px] w-[350px] mx-auto gap-2'></div>
+    </form>
+  )
+}
